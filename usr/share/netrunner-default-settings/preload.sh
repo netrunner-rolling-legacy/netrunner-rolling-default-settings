4610bbd051a201071ce5fc30be7f926789f5e797
Leszek Lesner <leszek.lsner@web.de>
2014-04-03 11:49:42 +0200

    Added check for 2 GB RAM before preloading firefox

--- usr/share/netrunner-default-settings/preload.sh
+++ usr/share/netrunner-default-settings/preload.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-sleep 2;
+
+# Make sure there is no lock file no matter what on start
 rm -f ~/.mozilla/firefox/*/lock
-ksystraycmd --hidden firefox &
+
+TotalMem=$(cat /proc/meminfo | grep -i memtotal | cut -d ':' -f 2 | awk '{print $1}')
+# Under 2 GB of RAM means skip preloading
+if [ "$TotalMem" -le "2000000" ]; then
+  echo "Preloading skipped as under 2 GB of RAM detected"
+  exit 0
+else 
+  sleep 2;
+  ksystraycmd --hidden firefox &
+fi
