54f91b547d80726b209a37f24e1fb34625c108a1
James Kittsmiller <jameskittsmiller@gmail.com>
2015-09-18 20:46:35 -0400

    Merged two packages into one.
    Merging of netreunner-rolling-system-settings and netrunner-rolling-
    default-settings and setting of defaults to be more like Netrunner
    Standard.

---
+++ usr/share/netrunner-default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -0,0 +1,69 @@
+var panel = new Panel
+var panelScreen = panel.screen
+var freeEdges = {"bottom": true, "top": true, "left": true, "right": true}
+
+for (i = 0; i < panelIds.length; ++i) {
+    var tmpPanel = panelById(panelIds[i])
+    if (tmpPanel.screen == panelScreen) {
+        // Ignore the new panel
+        if (tmpPanel.id != panel.id) {
+            freeEdges[tmpPanel.location] = false;
+        }
+    }
+}
+
+if (freeEdges["bottom"] == true) {
+    panel.location = "bottom";
+} else if (freeEdges["top"] == true) {
+    panel.location = "top";
+} else if (freeEdges["left"] == true) {
+    panel.location = "left";
+} else if (freeEdges["right"] == true) {
+    panel.location = "right";
+} else {
+    // There is no free edge, so leave the default value
+    panel.location = "top";
+}
+
+panel.height = screenGeometry(panel.screen).height > 1024 ? 35 : 35
+
+var kicker = panel.addWidget("org.kde.plasma.kicker")
+kicker.currentConfigGroup = ["Shortcuts"]
+kicker.writeConfig("global", "Alt+F1")
+
+kicker.currentConfigGroup = ["General"]
+kicker.writeConfig("customButtonImage", "file:///usr/share/pixmaps/homerun.svgz")
+kicker.writeConfig("favoriteApps", ["systemsettings.desktop",
+"org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
+kicker.writeConfig("limitDepth", true)
+kicker.writeConfig("useCustomButtonImage", true)
+
+var eitm = panel.addWidget("org.kde.plasma.expandingiconstaskmanager")
+eitm.currentConfigGroup = ["Configuration", "General"]
+eitm.writeConfig("launchers" ,"file:///usr/share/applications/org.kde.dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/firefox.desktop")
+eitm.writeConfig("showOnlyCurrentDesktop", true)
+
+var yakuakeIcon = panel.addWidget("org.kde.plasma.icon")
+yakuakeIcon.currentConfigGroup = ["General"]
+yakuakeIcon.writeConfig("applicationName", "Yakuake")
+yakuakeIcon.writeConfig("genericName", "Drop-down Terminal")
+yakuakeIcon.writeConfig("iconName", "yakuake")
+yakuakeIcon.writeConfig("url", "/usr/share/applications/kde4/yakuake.desktop")
+
+var pvolumemixer = panel.addWidget("org.kde.plasma.volume")
+
+var systray = panel.addWidget("org.kde.plasma.systemtray")
+systray.currentConfigGroup = ["General"]
+systray.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.battery,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard,org.kde.plasma.printmanager,org.kde.ktp-contactlist,org.kde.plasma.ktplegacypresenceapplet,org.kde.muonnotifier,org.kde.plasma.volume,org.kde.muonnotifier")
+systray.writeConfig("knownItems","org.kde.plasma.networkmanagement,org.kde.plasma.notifications,org.kde.plasma.volume,org.kde.plasma.bluetooth,org.kde.plasma.printmanager,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.battery,org.kde.ktp-contactlist,org.kde.plasma.clipboard")
+systray.writeConfig("hiddenItems","org.kde.plasma.clipboard,org.kde.plasma.volume,kmix")
+systray.writeConfig("shownItems", "transmission")
+
+panel.addWidget("org.kde.plasma.digitalclock")
+panel.addWidget("org.kde.plasma.notifications")
+
+var krunnerIcon = panel.addWidget("org.kde.plasma.icon")
+krunnerIcon.currentConfigGroup = ["General"]
+krunnerIcon.writeConfig("applicationName", "Krunner")
+krunnerIcon.writeConfig("iconName", "applications-system")
+krunnerIcon.writeConfig("url", "/usr/share/applications/krunner.desktop")
---
+++ usr/share/netrunner-default-settings/plasma5-profile/plasma/shells/org.kde.plasma.desktop/contents/layout.js
@@ -0,0 +1,68 @@
+
+loadTemplate("org.kde.plasma.desktop.defaultPanel")
+
+// for (var i = 0; i < screenCount; ++i) {
+//     var id = createActivity("Desktop");
+//     var desktopsArray = desktopsForActivity(id);
+//     print(desktopsArray.length);
+//     for( var j = 0; j < desktopsArray.length; j++) {
+//         desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+//         //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
+//     }
+// }
+
+var actionPlugins = ConfigFile("plasma-org.kde.plasma.desktop-appletsrc", "ActionPlugins");
+
+var actionPluginsDesktop = ConfigFile(actionPlugins,"0");
+actionPluginsDesktop.writeEntry("MidButton;NoModifier", "org.kde.paste");
+actionPluginsDesktop.writeEntry("RightButton;NoModifier", "org.kde.contextmenu");
+actionPluginsDesktop.writeEntry("wheel:Vertical;NoModifier", "org.kde.switchdesktop");
+
+var rightButton = ConfigFile(actionPluginsDesktop,"RightButton;NoModifier");
+rightButton.writeEntry("_add panel", "true");
+rightButton.writeEntry("_context", "true");
+rightButton.writeEntry("_lock_screen", "false");
+rightButton.writeEntry("_logout", "false");
+rightButton.writeEntry("_run_command", "true");
+rightButton.writeEntry("_sep1", "false");
+rightButton.writeEntry("_sep2", "true");
+rightButton.writeEntry("_sep3", "false");
+rightButton.writeEntry("_wallpaper", "true");
+rightButton.writeEntry("add sibling containment", "false");
+rightButton.writeEntry("add widgets", "true");
+rightButton.writeEntry("configure", "true");
+rightButton.writeEntry("configure shortcuts", "false");
+rightButton.writeEntry("lock widgets", "true");
+rightButton.writeEntry("manage activities", "false");
+rightButton.writeEntry("remove", "true");
+rightButton.writeEntry("run associated application", "false");
+
+delete rightButton;
+delete actionPluginsDesktop;
+delete actionPlugins;
+gc(); 
+
+//loadTemplate("org.kde.plasma-desktop.defaultPanel");
+//loadTemplate("org.kde.plasma-desktop.sidebarPanel");
+
+for (var i = 0; i < screenCount; ++i) {
+    var id = createActivity("Netrunner Desktop", "org.kde.plasma.folder")
+    var desktopsArray = desktopsForActivity(id);
+    
+    for (var j = 0; j < desktopsArray.length; j++) {
+        desktopsArray[j].wallpaperPlugin = 'org.kde.image'
+        desktopsArray[j].wallpaperMode = 'SingleImage'
+
+        desktopsArray[j].currentConfigGroup = new Array("General");
+        desktopsArray[j].writeConfig("positions","1,13,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/Readme.desktop,0,2,desktop:/calamares.desktop,0,3")
+        desktopsArray[j].writeConfig("pressToMove",true);
+        desktopsArray[j].writeConfig("showToolbox",false);
+        desktopsArray[j].writeConfig("popups",false);
+        desktopsArray[j].writeConfig("selectionMarkers",false);
+        desktopsArray[j].writeConfig("sortMode","-1");
+//         [Containments][18][Wallpaper][org.kde.image][General]
+        desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
+        desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/rolling-wallpaper-7.png");
+        desktopsArray[j].writeConfig("FillMode","2");
+    }
+}
